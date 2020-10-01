0628f216feab39026dbc56c6d0d9e0a8659bb46f
Vijay Bhaskar Reddy M <vreddy@thoughtworks.com>
2020-10-02 02:26:31 +0530

    Added Postgres Database

--- src/app.js
+++ src/app.js
@@ -12,4 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
 
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(400).json({ error: err.stack });
+});
+
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { indexPage, messagesPage } from '../controllers';
+import { indexPage, messagesPage, addMessage } from '../controllers';
+import { modifyMessage, performAsyncAction } from '../middleware';
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 
 export default indexRouter;
